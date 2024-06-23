ac516f4f751b43a1898b7337df0bee57464c1df1
ohnahee <ohnahee0824@gmail.com>
2024-06-23 22:49:08 +0900

    for i in range(1, n+1):

---
+++ Day_7/baekjoon_11021.py
@@ -0,0 +1,5 @@
+n = int(input())
+
+for i in range(1, n+1): #1부터 n까지의 숫자를 차례대로 i에 대입하며 반복문 내부코드 실행
+    numbers = list(map(int, input().split()))
+    print("Case #%d: %d" % (i, numbers[0] + numbers[1]))
