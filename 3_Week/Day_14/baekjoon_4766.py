4240eedbe75f4dc1d3efaf3efba995b1536f0a8c
ohnahee <ohnahee0824@gmail.com>
2024-07-01 22:47:03 +0900

    :.2f 소숫점 자리 나타내기 포맷

---
+++ 3_Week/Day_14/baekjoon_4766.py
@@ -0,0 +1,12 @@
+tem = []
+
+while True:
+    value = float(input())
+    if value == 999:
+        break
+    tem.append(value)
+
+n = len(tem)
+
+for i in range(n - 1):
+    print(f"{tem[i + 1] - tem[i]:.2f}")
