826ea1b8d1a65c7ddd66a4d37cf88f51afd3cba1
ohnahee <ohnahee0824@gmail.com>
2024-07-10 20:09:01 +0900

    map 활용

---
+++ 4_Week/baekjoon_2588.py
@@ -0,0 +1,17 @@
+# Day 23
+
+a, b, c = map(int, input())
+d, e, f = map(int, input())
+
+abc = a * 100 + b * 10 + c
+d_e_f = d * 100 + e * 10 + f
+
+A = abc * f
+B = abc * e
+C = abc * d
+D = A + (B * 10) + (C * 100)
+
+print(A)
+print(B)
+print(C)
+print(D)
