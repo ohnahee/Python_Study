609a592421307ec1623813e8169808673efa773c
ohnahee <ohnahee0824@gmail.com>
2024-07-17 21:45:47 +0900

    commit

---
+++ 5_Week/baekjoon_2480.py
@@ -0,0 +1,17 @@
+# Day 30
+
+A, B, C = map(int, input().split())
+money = 0
+
+if A == B == C:
+    money = 10000 + A * 1000
+elif A != B and B != C and C != A:
+    max_number = max(A, B, C)
+    money = max_number * 100 
+else:
+    if A == B or A == C:
+        money = 1000 + A * 100
+    else:
+        money = 1000 + B * 100
+
+print(money)
---
+++ 5_Week/baekjoon_2752.py
@@ -0,0 +1,7 @@
+# Day 29
+
+numbers = list(map(int, input().split()))
+
+sorted_numbers = sorted(numbers)
+
+print(" ".join(map(str, sorted_numbers)))
